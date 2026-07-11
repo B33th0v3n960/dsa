29bce6fea8b104abf299a0124ba29f4d5f70c8ed
B33th0v3n960 <zhousun06@gmail.com>
2026-07-11 11:26:40 +0100

    feat: create print float array function

--- include/utils/array_utils.h
+++ include/utils/array_utils.h
@@ -1,8 +1,10 @@
 #ifndef ARRAY_UTILS_H
 #define ARRAY_UTILS_H
 
-#define array_len(array) (sizeof(array) / sizeof(array[0]));
+#include <stddef.h>
 
-void print_array_int(int array[], int array_length);
+#define array_len(array) (sizeof(array) / sizeof(array[0]));
+void print_array_int(int array[], size_t array_length);
+void print_array_float(float array[], size_t array_length);
 
 #endif // !ARRAY_UTILS_H
--- src/utils/array_utils.c
+++ src/utils/array_utils.c
@@ -1,9 +1,17 @@
 #include "utils/array_utils.h"
+#include <stddef.h>
 #include <stdio.h>
 
-/* A utility function to print array of size n */
-void print_array_int(int array[], int array_length) {
-    for (int i = 0; i < array_length; ++i)
+/** A utility function to print int array of size n */
+void print_array_int(int array[], size_t array_length) {
+    for (size_t i = 0; i < array_length; i++)
         printf("%d ", array[i]);
+    printf("\n");
+}
+
+/** A utility function to print float array of size n */
+void print_array_float(float array[], size_t array_length) {
+    for (size_t i = 0; i < array_length; i++)
+        printf("%f ", array[i]);
     printf("\n");
 }
