76b740b0256a7212725f0ee779be786cc12534e1
B33th0v3n960 <zhousun06@gmail.com>
2026-07-17 16:32:22 +0100

    feat: created linkedlist_append_int and linkedlist_prepend_int

--- src/data_structures/linkedlist_int.c
+++ src/data_structures/linkedlist_int.c
@@ -14,10 +14,11 @@ struct LinkedList_int {
 };
 
 /**
- * @brief Creates a LinkedList on the heap
+ * Creates a LinkedList on the heap.
  *
- * @retval `LinkedList_int`   on success
- * @retval NULL             if allocation fails
+ * Retval:
+ * `LinkedList_int`   on success
+ * `NULL`             if allocation fails
  */
 LinkedList_int linkedlist_create_int(void) {
     LinkedList_int list = calloc(1, sizeof(struct LinkedList_int));
@@ -25,9 +26,7 @@ LinkedList_int linkedlist_create_int(void) {
 }
 
 /**
- * @brief Free the `LinkedList_int` from memory including each list items
- *
- * @param [list] The `LinkedList_int` to free
+ *  Free the `LinkedList_int` from memory including each list items.
  */
 void linkedlist_free_int(LinkedList_int list) {
     while (list->head != NULL) {
@@ -36,4 +35,58 @@ void linkedlist_free_int(LinkedList_int list) {
     }
     list->tail = NULL;
     free(list);
+}
+
+/**
+ * Append a new `Node_int`, with value of `new_value`, to the end of `list`.
+ *
+ * Retval:
+ * - `0`    on success
+ * - `-1`   if fails
+ */
+int linkedlist_append_int(LinkedList_int list, int new_value) {
+    if (list == NULL) return -1;
+
+    struct Node_int *new_node = calloc(1, sizeof(struct Node_int));
+    if (new_node == NULL) return -1;
+    new_node->data = new_value;
+
+    if (list->head == NULL) {
+        list->head = list->tail = new_node;
+        list->length = 1;
+        return 0;
+    }
+
+    list->tail->next = new_node;
+    new_node->prev = list->tail;
+    list->tail = new_node;
+    list->length++;
+    return 0;
+}
+
+/**
+ * Prepend a new `Node_int`, with value of `new_value`, to the start of `list`.
+ *
+ * Retval:
+ * - `0`    on success
+ * - `-1`   if fails
+ */
+int linkedlist_prepend_int(LinkedList_int list, int new_value) {
+    if (list == NULL) return -1;
+
+    struct Node_int *new_node = calloc(1, sizeof(struct Node_int));
+    if (new_node == NULL) return -1;
+    new_node->data = new_value;
+
+    if (list->head == NULL) {
+        list->head = list->tail = new_node;
+        list->length = 1;
+        return 0;
+    }
+
+    list->head->prev = new_node;
+    new_node->next = list->head;
+    list->head = new_node;
+    list->length++;
+    return 0;
 }
