63dc6c6d3651c01ba56a695250d7610f42226dfa
B33th0v3n960 <zhousun06@gmail.com>
2026-07-17 20:47:01 +0100

    test: create prepend tests

--- tests/test_linkedlist_int.c
+++ tests/test_linkedlist_int.c
@@ -49,10 +49,45 @@ void test_append_ten_values(void) {
     TEST_ASSERT_EQUAL_INT(expected, actual);
 }
 
+void test_prepend_int_value(void) {
+    int actual, expected;
+    expected = 0;
+    actual = linkedlist_prepend_int(list, 5);
+    printf("Actual: %d\n", actual);
+    printf("Expected: %d\n", expected);
+
+    TEST_ASSERT_EQUAL_INT(expected, actual);
+}
+
+void test_prepend_to_null(void) {
+    int actual, expected;
+    expected = -1;
+    actual = linkedlist_prepend_int(NULL, 5);
+    printf("Actual: %d\n", actual);
+    printf("Expected: %d\n", expected);
+
+    TEST_ASSERT_EQUAL_INT(expected, actual);
+}
+
+void test_prepend_ten_values(void) {
+    int actual, expected;
+    expected = 0;
+
+    for (int i = 0; i < 10; i++)
+        actual = linkedlist_prepend_int(list, 5);
+    printf("Actual: %d\n", actual);
+    printf("Expected: %d\n", expected);
+
+    TEST_ASSERT_EQUAL_INT(expected, actual);
+}
+
 int main(void) {
     UNITY_BEGIN();
     RUN_TEST(test_append_int_value);
     RUN_TEST(test_append_to_null);
     RUN_TEST(test_append_ten_values);
+    RUN_TEST(test_prepend_int_value);
+    RUN_TEST(test_prepend_to_null);
+    RUN_TEST(test_prepend_ten_values);
     return UNITY_END();
 }
