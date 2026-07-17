c710d2b962464f1bc17f0ac2ace198f50a2dfa96
B33th0v3n960 <zhousun06@gmail.com>
2026-07-17 12:20:11 +0100

    feat: created linkedlist_create_int and linkedlist_free_int

--- src/data_structures/linkedlist_int.c
+++ src/data_structures/linkedlist_int.c
@@ -1,4 +1,5 @@
 #include "data_structures/linkedlist_int.h"
+#include <stdlib.h>
 
 struct Node_int {
     int data;
@@ -11,3 +12,28 @@ struct LinkedList_int {
     struct Node_int *tail;
     int length;
 };
+
+/**
+ * @brief Creates a LinkedList on the heap
+ *
+ * @retval `LinkedList_int`   on success
+ * @retval NULL             if allocation fails
+ */
+LinkedList_int linkedlist_create_int(void) {
+    LinkedList_int list = calloc(1, sizeof(struct LinkedList_int));
+    return list;
+}
+
+/**
+ * @brief Free the `LinkedList_int` from memory including each list items
+ *
+ * @param [list] The `LinkedList_int` to free
+ */
+void linkedlist_free_int(LinkedList_int list) {
+    while (list->head != NULL) {
+        list->head = list->head->next;
+        free(list->head->prev);
+    }
+    list->tail = NULL;
+    free(list);
+}
