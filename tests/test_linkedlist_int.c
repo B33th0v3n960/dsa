581621462a879b113b8d0a0300d92975f06e10e6
B33th0v3n960 <zhousun06@gmail.com>
2026-07-18 15:51:06 +0100

    test: create test_append_int_value_and_pop and test_append_ten_values_and_pop

--- tests/test_linkedlist_int.c
+++ tests/test_linkedlist_int.c
@@ -42,7 +42,7 @@ void test_append_ten_values(void) {
     expected = 0;
 
     for (int i = 0; i < 10; i++)
-        actual = linkedlist_append_int(list, 5);
+        actual = linkedlist_append_int(list, i);
     printf("Actual: %d\n", actual);
     printf("Expected: %d\n", expected);
 
@@ -74,11 +74,47 @@ void test_prepend_ten_values(void) {
     expected = 0;
 
     for (int i = 0; i < 10; i++)
-        actual = linkedlist_prepend_int(list, 5);
+        actual = linkedlist_prepend_int(list, i);
     printf("Actual: %d\n", actual);
     printf("Expected: %d\n", expected);
 
     TEST_ASSERT_EQUAL_INT(expected, actual);
+}
+
+void test_append_int_value_and_pop(void) {
+    int actual, expected, append_value, actual_pop_value, expected_pop_value;
+    expected = 0;
+    append_value = 5;
+
+    actual = linkedlist_append_int(list, append_value);
+    printf("Actual: %d\n", actual);
+    printf("Expected: %d\n", expected);
+    TEST_ASSERT_EQUAL_INT(expected, actual);
+
+    expected = 0;
+    expected_pop_value = append_value;
+    actual = linkedlist_pop_int(list, &actual_pop_value);
+    printf("Pop value: %d\n", actual_pop_value);
+    TEST_ASSERT_EQUAL_INT(expected, actual);
+    TEST_ASSERT_EQUAL_INT(expected_pop_value, actual_pop_value);
+}
+
+void test_append_ten_values_and_pop(void) {
+    int actual, expected, actual_pop_value, expected_pop_value;
+    expected = 0;
+
+    for (int i = 0; i < 10; i++)
+        actual = linkedlist_append_int(list, i);
+    printf("Actual: %d\n", actual);
+    printf("Expected: %d\n", expected);
+    TEST_ASSERT_EQUAL_INT(expected, actual);
+
+    expected = 0;
+    expected_pop_value = 9;
+    actual = linkedlist_pop_int(list, &actual_pop_value);
+    printf("Pop value: %d\n", actual_pop_value);
+    TEST_ASSERT_EQUAL_INT(expected, actual);
+    TEST_ASSERT_EQUAL_INT(expected_pop_value, actual_pop_value);
 }
 
 int main(void) {
@@ -89,5 +125,7 @@ int main(void) {
     RUN_TEST(test_prepend_int_value);
     RUN_TEST(test_prepend_to_null);
     RUN_TEST(test_prepend_ten_values);
+    RUN_TEST(test_append_int_value_and_pop);
+    RUN_TEST(test_append_ten_values_and_pop);
     return UNITY_END();
 }
